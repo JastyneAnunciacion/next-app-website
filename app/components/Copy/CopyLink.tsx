b39b8bc2b445d408ea290c17639a9bcb0243a98e
Jastyne <jastyneraphaelanunciacion@gmail.com>
2024-08-26 19:16:47 +0800

    fix copy link issue

--- app/components/Copy/CopyLink.tsx
+++ app/components/Copy/CopyLink.tsx
@@ -10,7 +10,9 @@ const CopyLink = ({ textToCopy }: CopyLinkProps) => {
     return (
         <div className='border border-[#4B348C] rounded-md bg-[#170A35] text-[#AEA8BE] flex items-center gap-[3.25%] pl-[4.87%] pr-[1.62%] py-[1.62%]'>
             <ImageResponsive src='/images/purple-chain-image.png' alt='Chain Link Icon' width='4.87%' />
-            <CopyTextTemplate textToCopy={textToCopy} />
+            <div className='w-full flex justify-between items-center'>
+                <CopyTextTemplate textToCopy={textToCopy} />
+            </div>
         </div>
     )
 }
