5b6274c15659c84c3cf5353bf04663656223c38b
Jastyne <jastyneraphaelanunciacion@gmail.com>
2024-09-12 15:11:07 +0800

    fix some more stuff on right side

--- app/components/SideBar/RightSideBar/ChatBar/ChatBar.tsx
+++ app/components/SideBar/RightSideBar/ChatBar/ChatBar.tsx
@@ -7,33 +7,31 @@ interface ChatBarProps {
 
 const ChatBar = ({ show = true }: ChatBarProps) => {
     return (
-        <>
-            <div className={`shrink-0 fixed transition-width duration-300 w-[17.71vw] max-w-[340px] bg-[#070123] h-full flex flex-col text-white ${!show && 'translate-x-full'}`}>
-                <p
-                    style={{ fontSize: 'clamp(1px, 0.97vw , 18.68px)' }}
-                    className='pl-[9.80%] mb-[3.92%] pt-[3.92%]'
-                >
-                    Chat
-                </p>
-                <div
-                    style={{
-                        WebkitOverflowScrolling: 'touch',
-                        scrollbarWidth: 'none',
-                        msOverflowStyle: 'none',
-                        overflowY: 'scroll',
-                        overflowX: 'visible',
-                    }}
-                    className='pl-[5.88%] pr-[3.92%]'
-                >
-                    {Array.from({ length: 20 }).map((_, index) => (
-                        <div className={`w-full aspect-[115/33] ${index != 19 && 'mb-[2.17%]'}`} key={index}>
-                            <ChatBarMessage key={index} />
-                        </div>
-                    ))}
-                </div>
-                <ChatBarChatBox />
-            </div >
-        </>
+        <div className={`shrink-0 fixed transition-width duration-300 w-[17.71vw] max-w-[340px] bg-[#070123] h-[90vh] flex flex-col text-white ${!show && 'translate-x-full'}`}>
+            <p
+                style={{ fontSize: 'clamp(1px, 0.97vw , 18.68px)' }}
+                className='pl-[9.80%] mb-[3.92%] pt-[3.92%]'
+            >
+                Chat
+            </p>
+            <div
+                style={{
+                    WebkitOverflowScrolling: 'touch',
+                    scrollbarWidth: 'none',
+                    msOverflowStyle: 'none',
+                    overflowY: 'scroll',
+                    overflowX: 'visible',
+                }}
+                className='pl-[5.88%] pr-[3.92%]'
+            >
+                {Array.from({ length: 21 }).map((_, index) => (
+                    <div className={`w-full aspect-[115/33] ${index != 20 && 'mb-[2.17%]'}`} key={index}>
+                        <ChatBarMessage key={index} />
+                    </div>
+                ))}
+            </div>
+            <ChatBarChatBox />
+        </div >
     )
 }
 
--- app/components/SideBar/RightSideBar/NotificationBar/NotificationBar.tsx
+++ app/components/SideBar/RightSideBar/NotificationBar/NotificationBar.tsx
@@ -7,32 +7,30 @@ interface NotificationBarProps {
 
 const NotificationBar = ({ show }: NotificationBarProps) => {
     return (
-        <>
-            <div className={`shrink-0 fixed transition-width duration-300 w-[17.71vw] max-w-[340px] bg-[#070123] h-full flex flex-col text-white ${!show && 'translate-x-full'}`}>
-                <p
-                    style={{ fontSize: 'clamp(1px, 0.97vw , 18.68px)' }}
-                    className='pl-[9.80%] mb-[3.92%] pt-[3.92%]'
-                >
-                    Notification
-                </p>
-                <div
-                    style={{
-                        WebkitOverflowScrolling: 'touch',
-                        scrollbarWidth: 'none',
-                        msOverflowStyle: 'none',
-                        overflowY: 'scroll',
-                        overflowX: 'visible',
-                    }}
-                    className='pl-[5.88%] pr-[3.92%]'
-                >
-                    {Array.from({ length: 10 }).map((_, index) => (
-                        <div className={`w-full aspect-[23/24] ${index != 9 && 'mb-[2.17%]'}`} key={index}>
-                            <Notification />
-                        </div>
-                    ))}
-                </div>
+        <div className={`shrink-0 fixed transition-width duration-300 w-[17.71vw] max-w-[340px] bg-[#070123] h-[90vh] flex flex-col text-white ${!show && 'translate-x-full'}`}>
+            <p
+                style={{ fontSize: 'clamp(1px, 0.97vw , 18.68px)' }}
+                className='pl-[9.80%] mb-[3.92%] pt-[3.92%]'
+            >
+                Notification
+            </p>
+            <div
+                style={{
+                    WebkitOverflowScrolling: 'touch',
+                    scrollbarWidth: 'none',
+                    msOverflowStyle: 'none',
+                    overflowY: 'scroll',
+                    overflowX: 'visible',
+                }}
+                className='pl-[5.88%] pr-[3.92%]'
+            >
+                {Array.from({ length: 10 }).map((_, index) => (
+                    <div className={`w-full aspect-[23/24] ${index != 9 && 'mb-[2.17%]'}`} key={index}>
+                        <Notification />
+                    </div>
+                ))}
             </div>
-        </>
+        </div>
     )
 }
 
